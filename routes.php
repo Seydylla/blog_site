694a608a40a3ecb889fdda6cac36c1e3805b9678
GamerInTheGame14 <seydyllagurbangeldiyew.07@gmail.com>
2026-08-03 23:52:47 +0500

    1.7.7

--- routes.php
+++ routes.php
@@ -6,13 +6,13 @@ $router->get('/about', 'controllers/about.php');
 $router->get('/article', 'controllers/articles/show.php');
 $router->delete('/article', 'controllers/articles/destroy.php');
 
-$router->get('/article/edit', 'controllers/articles/edit.php');
-$router->patch('/article', 'controllers/articles/update.php');
+$router->get('/article/edit', 'controllers/articles/edit.php')->only('auth');
+$router->patch('/article', 'controllers/articles/update.php')->only('auth');
 
-$router->get('/articles/create', 'controllers/articles/create.php');
-$router->post('/articles/create', 'controllers/articles/store.php');
+$router->get('/articles/create', 'controllers/articles/create.php')->only('auth');
+$router->post('/articles/create', 'controllers/articles/store.php')->only('auth');
 
 $router->get('/articles', 'controllers/articles/index.php');
 
-$router->get('/register', 'controllers/registration/create.php');
-$router->post('/register', 'controllers/registration/store.php');
+$router->get('/register', 'controllers/registration/create.php')->only('guest');
+$router->post('/register', 'controllers/registration/store.php')->only('guest');
